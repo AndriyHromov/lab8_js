3299942fe28d4909fde52fc904790d7451f75614
AndriyHromov <146163843+AndriyHromov@users.noreply.github.com>
2026-05-11 11:56:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const burger = document.getElementById("burger");
+const navMenu = document.getElementById("navMenu");
+
+burger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
+
+const slides = document.getElementById("slides");
+const dots = document.querySelectorAll(".dot");
+
+let index = 0;
+
+function showSlide(i) {
+    index = i;
+
+    slides.style.transform = `translateX(-${index * 100}%)`;
+
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+}
+
+document.getElementById("next").addEventListener("click", () => {
+    index++;
+    if(index > 2) index = 0;
+    showSlide(index);
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    index--;
+    if(index < 0) index = 2;
+    showSlide(index);
+});
+
+dots.forEach(dot => {
+    dot.addEventListener("click", () => {
+        showSlide(Number(dot.dataset.index));
+    });
+});
+
+setInterval(() => {
+    index++;
+    if(index > 2) index = 0;
+    showSlide(index);
+}, 3000);
